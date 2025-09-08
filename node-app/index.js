67bb2a9bc7a6becaf7a44035607947819e4549c2
Kosikan Nagananthakumaran <150826559+kosikan20@users.noreply.github.com>
2025-09-08 14:34:14 +0530

    Update index.js

--- node-app/index.js
+++ node-app/index.js
@@ -1,1 +1,13 @@
-console.log('hello from node-app/index.js');
+const express = require("express");
+const app = express();
+const PORT = 3000;
+
+// simple route
+app.get("/", (req, res) => {
+  res.send("Hello from Express server!");
+});
+
+// bind to all interfaces
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Express server running on port ${PORT}`);
+});
